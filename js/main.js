a9b8ec71780e3290f2cf6d470c878fe8a5f99427
rebs-code <jack.rebussi@gmail.com>
2023-12-05 18:18:25 +0100

    feat: email generation functionality

--- js/main.js
+++ js/main.js
@@ -7,7 +7,25 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      emailAPI: "https://flynn.boolean.careers/exercises/api/random/mail",
+      emails: [],
     };
+  },
+  methods: {
+    getEmail() {
+      const emailPromise = [];
+      for (let i = 0; i < 10; i++) {
+        emailPromise.push(
+          axios.get(this.emailAPI).then((resp) => {
+            if (emailPromise.includes(resp.data.response)) {
+              i--;
+            } else return resp.data.response;
+          })
+        );
+      }
+      Promise.all(emailPromise).then((results) => {
+        this.emails = results;
+      });
+    },
   },
 }).mount("#app");
